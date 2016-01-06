e6fa5dcbb4d7deba869761752f2819bd6ce5e818
Rao (Katie) Xu <dnait@users.noreply.github.com>
2016-01-05 19:17:17 -0500

    Update myrpn.java

--- myrpn.java
+++ myrpn.java
@@ -8,37 +8,35 @@ import java.util.regex.*;
 
 import javax.swing.JOptionPane;
 public class myrpn {
-
     public long stringToArithmetic(String string) {
     	double temp= suffixToArithmetic(infixToSuffix(string));
     	long x = (long)temp; 
-     return x;
+        return x;
     }
   
     public static String infixToSuffix(String infix) {
-     
         Stack< Character> stack = new Stack< Character>();
         String suffix = "";
         int length = infix.length();
         for (int i = 0; i < length; i++) {
-        	
-        	
+
             Character temp ;
             char c = infix.charAt(i);
-               
             switch (c) {
-           
+                
             case '(':
                 stack.push(c);
                 break;
-           
+                
             case '+':
-            	
+            	 if ( infix.charAt(i)=='+'&&( i==0||infix.charAt(i-1)=='(')){
+            	    suffix += "0" + "";
+                }
+                
             case '-':
-            if ( infix.charAt(i)=='-'&&( i==0||infix.charAt(i-1)=='(')){
-            	 suffix += "0" + "";
-            }
-            
+                if ( infix.charAt(i)=='-'&&( i==0||infix.charAt(i-1)=='(')){
+            	    suffix += "0" + "";
+                }
                 while (stack.size() != 0) {
                     temp = stack.pop();
                     if (temp == '(') {
@@ -50,10 +48,8 @@ public class myrpn {
                 stack.push(c);
                 suffix += " ";
                 break;
-           
-            
+                
             case '*':
-            
                 while (stack.size() != 0) {
                     temp = stack.pop();
                     if (temp == '(' || temp == '+' || temp == '-') {
@@ -67,9 +63,7 @@ public class myrpn {
                 suffix += " ";
                 break;
                 
-           
             case '^':
-                
                 while (stack.size() != 0) {
                     temp = stack.pop();
                     if (temp == '*' ||temp == '(' || temp == '+' || temp == '-') {
@@ -83,68 +77,66 @@ public class myrpn {
                 suffix += " ";
                 break;
                 
-            
             case ')':
                 while (stack.size() != 0) {
                     temp = stack.pop();
-                    if (temp == '(')
+                    if (temp == '(') {
                         break;
-                    else
+                    } else {
                         suffix += " " + temp;
+                    }
                 }
-              
                 break;
                 
             default:
                 suffix += c;
             }
         }
-        while (stack.size() != 0)
+        while (stack.size() != 0) {
             suffix += " " + stack.pop();
+        }
         return suffix;
     }
     
-
     public static double suffixToArithmetic(String postfix) {
-    	
-    	
         Pattern pattern = Pattern.compile("\\d+"); 
         String strings[] = postfix.split(" ");
-        for (int i = 0; i < strings.length; i++)
-          strings[i].trim(); 
-       Stack< Double> stack = new Stack< Double>();
         for (int i = 0; i < strings.length; i++) {
-            
-            if (strings[i].equals(""))
+            strings[i].trim(); 
+        }
+        Stack< Double> stack = new Stack< Double>();
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].equals("")) {
                 continue;
+            }
             if ((pattern.matcher(strings[i])).matches()) {
-               
                 stack.push(Double.parseDouble(strings[i]));
             } else {
-               
                 double y = stack.pop();
                 double x = stack.pop();
                 stack.push(caculate(x, y, strings[i]));
             }
         }
         return stack.pop();
-      
-       
     }
+    
     private static double caculate(double x, double y, String simble) {
-        if (simble.trim().equals("+"))
+        if (simble.trim().equals("+")) {
             return x + y;
-        if (simble.trim().equals("-"))
+        }
+        if (simble.trim().equals("-")) {
             return x - y;
-        if (simble.trim().equals("*"))
+        }
+        if (simble.trim().equals("*")) {
             return x * y;
-        if (simble.trim().equals("^"))
-           { if (y>=0)       		
-            return Math.pow(x,y);
-           else
+        }
+        if (simble.trim().equals("^")) {
+            if (y>=0) {    		
+                return Math.pow(x,y);
+            } else {
         	   JOptionPane.showMessageDialog(null,"The power should be non-negative. Try again!");
-           
-        	   }
-        return 0;
+        	}
+            return 0;
+        }
     }
 }
