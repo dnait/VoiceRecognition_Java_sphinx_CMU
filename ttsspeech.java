0250334c99b16e9d76bdd63fa3d09624d8bea2d4
Rao (Katie) Xu <dnait@users.noreply.github.com>
2016-01-05 19:28:33 -0500

    Update ttsspeech.java

--- ttsspeech.java
+++ ttsspeech.java
@@ -2,21 +2,17 @@
 //  Copyright (c) 2014. All rights reserved.
 package sphinxTTScalculator
 
-
 import com.sun.speech.freetts.*;
-
 public class ttsspeech  {
-
-    public void speak(String mystring){       
-    final String VOICENAME="kevin16";
-                                    
+    public void speak(String mystring) {       
+        final String VOICENAME="kevin16";
         Voice voice;
         VoiceManager vm=VoiceManager.getInstance();
         voice=vm.getVoice(VOICENAME);
         voice.allocate();
         voice.speak(mystring);
     }
-    }
+}
 
 
 
