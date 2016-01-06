3029c43095ebbe31b31ff8ec121cc68c81b80339
Rao (Katie) Xu <dnait@users.noreply.github.com>
2016-01-05 19:27:34 -0500

    Update speech2text.java

--- speech2text.java
+++ speech2text.java
@@ -17,15 +17,13 @@ import java.io.BufferedWriter;
 
 
 public class speech2text {
-	
 	public String speaktotext(){
-		
 		ttsspeech speak2=new ttsspeech();
 		speak2.speak("This is Kavin, please tell me your expression.");
 
 
 		//System.out.println(mychar1);
-        String resultText;
+        	String resultText;
 		ConfigurationManager cm;
 		cm = new ConfigurationManager(speech2text.class.getResource("myword.config.xml"));
 		Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
@@ -34,9 +32,9 @@ public class speech2text {
 		// start the microphone or exit if the programm if this is not possible
 		Microphone microphone = (Microphone) cm.lookup("microphone");
 		if (!microphone.startRecording()) {
-		    System.out.println("Cannot start microphone.");
-		    recognizer.deallocate();
-		    System.exit(1);
+		    	System.out.println("Cannot start microphone.");
+		    	recognizer.deallocate();
+		    	System.exit(1);
 		}
 
 		//System.out.println("Waiting 5' and Say: ( one | two | three... )");
@@ -45,100 +43,96 @@ public class speech2text {
 		//String mystring="5";// loop the recognition until the programm exits.
 		String mychar="";
 		for (int i=0;i<100;i++){
-		    Result result = recognizer.recognize();
-		    resultText = result.getBestFinalResultNoFiller(); 
-		    if (result != null && resultText !="equal") {
+			Result result = recognizer.recognize();
+			resultText = result.getBestFinalResultNoFiller(); 
+			if (result != null && resultText !="equal") {
 		        //resultText = result.getBestFinalResultNoFiller(); 
-		        switch (resultText){
-		        case "zero":
-		        	resultText="0";mychar=mychar+resultText;
-		        	break;
-		        case "one":
-		        	resultText="1";mychar=mychar+resultText;
-		        	break;
-		        case "two":
-		        	resultText="2";mychar=mychar+resultText;
-		        	break;
-		        case "three":
-		        	resultText="3";mychar=mychar+resultText;
-		        	break;
-		        case "four":
-		        	resultText="4";mychar=mychar+resultText;
-		        	break;
-		        case "five":
-		        	resultText="5";mychar=mychar+resultText;
-		        	break;
-		        case "six":
-		        	resultText="6";mychar=mychar+resultText;
-		        	break;
-		        case "seven":
-		        	resultText="7";mychar=mychar+resultText;
-		        	break;
-		        case "eight":
-		        	resultText="8";mychar=mychar+resultText;
-		        	break;
-		        case "nine":
-		        	resultText="9";mychar=mychar+resultText;
-		        	break;
-		        case "add":
-		        	resultText="+";mychar=mychar+resultText;
-		        	break;
-		        case "minus":
-		        	resultText="-";mychar=mychar+resultText;
-		        case "multiply":
-		        	resultText="*";mychar=mychar+resultText;
-		        	break;
-		        case "power":
-		        	resultText="^";mychar=mychar+resultText;
-		        	break;
-		        case "left-brace":
-		        	resultText="(";mychar=mychar+resultText;
-		        	break;
-		        case "right-brace":
-		        	resultText=")";mychar=mychar+resultText;
-		        	break;
-		        case "backspace":
-		        	int leng=mychar.length();
-		        	if (leng==1){mychar="";System.out.println("No Input!");speak1.speak("No Input");}
-		        	else
-		        	mychar=mychar.substring(0,leng-1);
-		        	break;
-		        case "equal":
-		        	resultText="=";
-		    		myrpn myconvert=new myrpn();
-		    		long ans=myconvert.stringToArithmetic(mychar);	
-		        	mychar=mychar+resultText+(ans+"");
-		
-		        	i=99;
-		        	break;
-		        case "clear":
-		        	mychar="";
-		        	System.out.println("Cleared!");speak1.speak("Cleared");
-		        	System.exit(1);
-		        	break;
-		        case "close":
-		        	System.exit(0);
-		        	break;
-		    }
-		       
-		        System.out.println(mychar);
-		        //write into the file
-		        File file = new File("voice.txt");
-		        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file)))
-		        {
-		          bw.write(String.valueOf(mychar));
-		         }
-		           
-		        catch (IOException ex) {
-		            ex.printStackTrace();
-			    }    
-		  } 
+				switch (resultText){
+			  		case "zero":
+			 			resultText="0";mychar=mychar+resultText;
+			  			break;
+			  		case "one":
+		        			resultText="1";mychar=mychar+resultText;
+		        			break;
+			  		case "two":
+		        			resultText="2";mychar=mychar+resultText;
+		        			break;
+			 		case "three":
+				        	resultText="3";mychar=mychar+resultText;
+				        	break;
+					case "four":
+				        	resultText="4";mychar=mychar+resultText;
+				        	break;
+				        case "five":
+				        	resultText="5";mychar=mychar+resultText;
+				        	break;
+					case "six":
+					    	resultText="6";mychar=mychar+resultText;
+					      	break;
+					case "seven":
+					       	resultText="7";mychar=mychar+resultText;
+				        	break;
+				      	case "eight":
+		        			resultText="8";mychar=mychar+resultText;
+		        			break;
+					case "nine":
+		        			resultText="9";mychar=mychar+resultText;
+		        			break;
+		        		case "add":
+		        			resultText="+";mychar=mychar+resultText;
+		        			break;
+				      	case "minus":
+					     	resultText="-";mychar=mychar+resultText;
+				     	case "multiply":
+					     	resultText="*";mychar=mychar+resultText;
+					   	break;
+					case "power":
+		        			resultText="^";mychar=mychar+resultText;
+		        			break;
+			 		case "left-brace":
+		        			resultText="(";mychar=mychar+resultText;
+		        			break;
+		        		case "right-brace":
+		        			resultText=")";mychar=mychar+resultText;
+		        			break;
+		        		case "backspace":
+		        			int leng=mychar.length();
+		        			if (leng==1) {
+		        				mychar="";System.out.println("No Input!");speak1.speak("No Input");
+		        			} else {
+							mychar=mychar.substring(0,leng-1);
+		        			}
+		        			break;
+		        		case "equal":
+		        			resultText="=";
+		    				myrpn myconvert=new myrpn();
+		    				long ans=myconvert.stringToArithmetic(mychar);	
+		        			mychar=mychar+resultText+(ans+"");
+						i=99;
+		        			break;
+		        		case "clear":
+		        			mychar="";
+		        			System.out.println("Cleared!");
+		        			speak1.speak("Cleared");
+		        			System.exit(1);
+		        			break;
+		        		case "close":
+		        			System.exit(0);
+		        			break;
+		    		}
+		    		System.out.println(mychar);
+		        	//write into the file
+		    		File file = new File("voice.txt");
+		    		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+		        	 	bw.write(String.valueOf(mychar));
+		    		} catch (IOException ex) {
+		            		ex.printStackTrace();
+				}    
+			} 
 		}
-
 		return mychar;
-
-		}
-		} 
+	}
+} 
 
 
 
